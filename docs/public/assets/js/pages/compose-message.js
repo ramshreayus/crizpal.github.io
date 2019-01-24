1255ed017e962c51aae3273bbe8670c623ca6147
CRIZPAL <ram@crizpal.com>
2019-01-24 12:34:22 +0530

    changes

--- docs/public/assets/js/pages/compose-message.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function()
-{
-    $('#message-field').summernote();
-});
--- docs/resources/views/layouts/admin/footer.blade.php
+++
@@ -1,4 +0,0 @@
-
-<footer class="content-wrapper-footer">
-    &copy; Copyright 2018 Africa Innovation Market <span class="fa fa-heart"></span> by <a href="http://www.crizpal.com" target="_blank">Crizpal Technologies</a>.
-</footer>
